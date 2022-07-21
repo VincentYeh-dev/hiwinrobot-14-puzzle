e20f3ea6d391615a3f9742506bf24c8429187a9b
VincentYeh-dev <vincent3314534>
2022-07-21 12:46:11 +0800

    分離Puzzle3D,RealWorldCoordinate

---
+++ ExclusiveProgram/puzzle/Puzzle3D.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExclusiveProgram.puzzle
+{
+    public struct Puzzle3D
+    {
+        public int ID;
+        public RealWorldCoordinate coordinate;
+        public string Position;
+    };
+}
